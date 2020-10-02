ebe8da22ac37292669ce6bf5d6b600eabd128bdb
Ananya Gupta <ananya.it19@nsut.ac.in>
2020-10-02 21:02:03 +0530

    Passport.js Authorisation

--- index.js
+++ index.js
@@ -7,22 +7,52 @@ const expressLayouts = require("express-ejs-layouts");
 const path = require("path");
 const port = 8000;
 const db = require("./config/mongoose");
+const session = require("express-session");
+const passport = require("passport");
+const passportLocal = require("./config/passport-local-strategy");
 
+//Cookie doesn't get lost on creating another session
+const MongoStore = require("connect-mongo")(session);
 // const server = http.createServer();
 //Setting up Cookie parser
 app.use(cookieParser());
 
 app.use(express.urlencoded());
-app.use("/", require("./router"));
-
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "Views"));
 
 app.use(expressLayouts);
 app.set("layout extractStyles", true);
 app.set("layout extractScripts", true);
 
 app.use(express.static("./assets"));
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "Views"));
+//Mongo Store used to store session cookie in the database
+app.use(
+  session({
+    name: "sochioh",
+    secret: "vnaibrbgr",
+    resave: false,
+    cookie: {
+      maxAge: 1000 * 60 * 100, //in milliseconds
+    },
+    store: new MongoStore(
+      {
+        mongooseConnection: db,
+        autoRemove: "disabled",
+      },
+      //if connection doesn't happen
+      function (err) {
+        console.log(err || "mongo-store connection ok");
+      }
+    ),
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(passport.setAuthenticatedUser);
+app.use("/", require("./router"));
 
 //Express ends request instead of loading and loading
 app.listen(port, function (err) {
--- router/users.js
+++ router/users.js
@@ -1,13 +1,21 @@
 const express = require("express");
+const passport = require("passport");
 //const { runInNewContext } = require('vm');
 var router = express.Router();
 const UserController = require("../controller/UserController");
 console.log("User router loaded");
 router.get("/sign_in", UserController.signin);
 router.get("/sign_up", UserController.signup);
-router.get("/profile", UserController.profile);
+//Display profile only when logged in
+router.get("/profile", passport.checkAuthentication, UserController.profile);
 // where is the route /users/porfile ?? //ohh i see that's the problem jjust a sec
 router.post("/create", UserController.create);
-router.post("/create_session", UserController.createSession);
+//router.post("/create_session", UserController.createSession);
+router.post(
+  "/create_session",
+  passport.authenticate("local", { failureRedirect: "/users/sign_in" }),
+  UserController.createSession
+);
+router.get("/sign_out", UserController.destroySession);
 
 module.exports = router;
