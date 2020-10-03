63c569bba2a441cf09f05eaf9ae86ea8ed84104e
Ananya Gupta <ananya.it19@nsut.ac.in>
2020-10-04 00:08:15 +0530

    AJAX conversion complete

--- assets/js/home_posts.js
+++ assets/js/home_posts.js
@@ -15,9 +15,82 @@
             {
                 console.log(error.responseText)
             }
-        })
-      })
+        });
+      });
   }
-}
-//method to create in dom
 
+//method to create in dom
+let newPostDom = function (post){
+  return $(`<li id="post-${post._id}">
+  <p>
+  <small>
+    <a class="delete-post-buttton"></a>href="/posts/destroy/<%=post.id%>"> Delete Post </a>
+  </small>
+  ${ post.content}
+  <br />
+  <small> ${post.user.name}
+   </small>
+</p>
+<div class="post-comments">
+  <form id= "post-${post._id}-comments-form" action="/comments/create" method="POST">
+    <input
+      type="text"
+      name="content"
+      placeholder="Type here to add comment"
+    />
+    <input type="hidden" name="post" value="${ post.id }" />
+    <input type="submit" value="post comment" />
+  </form>
+
+  <div class="post-comment-list">
+    <ul id="post-comments-${post._id}">
+      <% for (comment of post.comments) { %> <%- include('_comment') %> <% }%>
+    </ul>
+  </div>
+</div>
+</li>`);
+};
+
+//method for deleting post from DOM
+let deletePost = function(deletelink)
+{
+  $(deletelink).click(function(e)
+  {
+    e.preventDefault();
+
+    $.ajax({
+      type:'get',
+      url: $(deletelink).prop('href'),
+      success: function(data) {
+        $(`post-${data.data.post_id}`).remove();
+        new Noty({
+          theme:"relax",
+          text:"Post Deleted",
+          type:"success",
+          layout:"topRight",
+          timeout: 1500,
+        }).show();
+      },
+      error: function (error){
+        console.log(error.responseText);
+      }
+    });
+  });
+};
+
+//Converting posts to AJAX
+let convertPostsToAjax = function () {
+  $("#posts-list-container>ul>li").each(function () {
+    let self = $(this);
+    let deleteButton = $(" .delete-post-button", self);
+    deletePost(deleteButton);
+
+    // get the post's id by splitting the id attribute
+    let postId = self.prop("id").split("-")[1];
+    new PostComments(postId);
+  });
+};
+
+createPost();
+convertPostsToAjax();
+}
