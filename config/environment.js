12d20b338969bd05137282142b1be2e5790dd478
Ananya Gupta <ananya.it19@nsut.ac.in>
2020-10-04 22:54:13 +0530

    Header and Sign in front

---
+++ config/environment.js
@@ -0,0 +1,12 @@
+const development = {
+  name: "development",
+  asset_path: "./assets",
+  session_cookie_key: "vnaibrbgr",
+  db: "account",
+};
+
+const production = {
+  name: "production",
+};
+
+module.exports = development;
