fe76097aabcf3a12d158413487547a1869b26292
Ananya Gupta <ananya.it19@nsut.ac.in>
2020-10-15 13:02:53 +0530

    ready to deploy

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,8 +1,7 @@
 const mongoose = require("mongoose");
 const env = require("./environment");
 mongoose.connect('mongodb+srv://Abcd:Ananya1123*@cluster0.psdtd.mongodb.net/<dbname>?retryWrites=true&w=majority');  //this link isnt added yet // localhost hata diya replacement ni daaliok..online atlas op abh..i
-//I created the cluster kal, i am lost ...koi nh..me krta hu//aapne heroku download kr li ?yes yes 
-const db = mongoose.connection;//project mein bhi added hai..nh samjhe 
+const db = mongoose.connection;
 
 db.on("error", console.error.bind(console, "Error connecting to MongoDB"));
 
@@ -12,4 +11,3 @@ db.once("open", function () {
 });
 
 module.exports= db;
-//kuchh user create kijiye //local host pe ??//localhost kiuye didn't get you
