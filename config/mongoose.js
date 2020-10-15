fdb3dcee98165fc62e6ad897e334ade264718333
Ananya Gupta <ananya.it19@nsut.ac.in>
2020-10-15 13:00:33 +0530

    Mongo Cloud configured

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 const env = require("./environment");
-mongoose.connect(`mongodb://localhost/${env.db}`);
-
-const db = mongoose.connection;
+mongoose.connect('mongodb+srv://Abcd:Ananya1123*@cluster0.psdtd.mongodb.net/<dbname>?retryWrites=true&w=majority');  //this link isnt added yet // localhost hata diya replacement ni daaliok..online atlas op abh..i
+//I created the cluster kal, i am lost ...koi nh..me krta hu//aapne heroku download kr li ?yes yes 
+const db = mongoose.connection;//project mein bhi added hai..nh samjhe 
 
 db.on("error", console.error.bind(console, "Error connecting to MongoDB"));
 
@@ -12,3 +12,4 @@ db.once("open", function () {
 });
 
 module.exports= db;
+//kuchh user create kijiye //local host pe ??//localhost kiuye didn't get you
--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const env = require('./config/environment');
 //Cookie parser
 const cookieParser = require("cookie-parser");
 const app = express();
-const port = 8000;
+const port = process.env.PORT || 8000;
 const expressLayouts = require("express-ejs-layouts");
 const path = require("path");
 
